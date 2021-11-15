b95c25ce08b0f693d7f93bf7b070ab7969daaea2
darkeden48 <dzalex72@list.ru>
2021-11-15 22:02:43 +0200

    timer-zero-stop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class CountdownTimer {
     constructor({ targetDate, selector }) {
       this.targetDate = targetDate.getTime();
       this.selector = selector.slice(1);
-  
+  console.log(selector)
       this.timer = document.querySelector(".timer");
       this.days = document.querySelector('[data-value="days"]');
       this.hours = document.querySelector('[data-value="hours"]');
@@ -10,22 +10,22 @@ class CountdownTimer {
       this.secs = document.querySelector('[data-value="secs"]');
   
       this.start();
+      this.intervalId = null;
     }
-  
     start() {
       this.timer.setAttribute("id", this.selector);
-  
       setInterval(() => {
         const currentTime = Date.now();
         const deltaTime = this.targetDate - currentTime;
         const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
-  
+        if(days<=0 && hours<=0 && mins<=0 && secs<=0){return}
         this.days.textContent = days;
         this.hours.textContent = hours;
         this.mins.textContent = mins;
         this.secs.textContent = secs;
+      
       }, 1000);
-    }
+    };
   
     getTimeComponents(time) {
       const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
@@ -39,8 +39,14 @@ class CountdownTimer {
       return String(value).padStart(2, "0");
     }
   }
-
   new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    targetDate: new Date('Nov 17 2021 00:00'),
   });
+
+  
+
+
+
+
+  
